6da26d265740dbac185af78b192e61ab44433ab0
Mccaryr <robert.mccary@yahoo.com>
2019-03-10 17:08:44 -0700

    changes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,37 +1,49 @@
 class TweetsController < ApplicationController
 
     get '/tweets' do
-        @tweets = Tweet.all 
+        if !logged_in? 
+            redirect '/login'
+        end 
+        @tweets = Tweet.all
+        @user=current_user(session) 
         erb :'tweets/tweets'
     end  
         
     
     get '/tweets/new' do
+        if !logged_in? 
+            redirect '/login'
+        end 
         erb :'tweets/new'
     
     end 
 
     post '/tweets' do
+        @user=current_user(session)
         tweet = Tweet.create(content: params[:content])
     redirect "/tweets/#{tweet.id}"
     end  
 
     get '/tweets/:id' do 
+        if !logged_in?(session) 
+            redirect '/login'
+        end 
         @tweet = Tweet.find_by(params[:id])
         erb :'/tweets/show_tweet'  
     end 
 
     get '/tweets/:id/edit' do
+        if !logged_in?(session) 
+            redirect '/login'
+        end 
         @tweet = Tweet.find_by(params[:id])
-        binding.pry  
+        
         erb :'tweets/edit_tweet'
     end 
 
     patch '/tweets/:id' do
-        binding.pry  
+         
     
     redirect "/tweets/#{tweet.id}"
     end 
-
-
-end
+end 
